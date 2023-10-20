36f1bb4ad7081c731d3b89eaab3fd60606dd0275
Shorif <shorifahamed571@gmail.com>
2023-10-20 18:29:52 +0600

    Extra two section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -30,7 +30,7 @@ console.log(brands);
 }
 </div>
             
-            <div className="grid md:grid-cols-2 gap-6 my-20 container mx-auto" >
+            <div className="hidden   gap-6 my-20 container mx-auto" >
                 {
                     products.map((product) =>
                         <ProductCard
@@ -44,8 +44,37 @@ console.log(brands);
 
             </div>
             <ContactUs></ContactUs>
-            <About></About>
-            <Team></Team>
+            <div className="space-y-4 my-10">
+                <h2 className="text-5xl text-center font-bold pt-10">FAQ</h2>
+                <p className="text-center text-xl font-semibold py-4">Here are some frequently asked questions (FAQ) and answers for our customer</p>
+            <div className="collapse collapse-plus bg-base-200 ">
+  <input type="radio" name="my-accordion-3" checked="checked" /> 
+  <div className="collapse-title text-xl font-medium">
+  How do I buy my bike on your website? 
+  </div>
+  <div className="collapse-content"> 
+    <p>To sell your bike, simply create an account on our website and click on the "Sell Your Bike button. Follow the prompts to provide information about your bike, upload photos, set a price, and list your bike for sale.</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" /> 
+  <div className="collapse-title text-xl font-medium">
+  Is there a fee to list my bike for sale?
+  </div>
+  <div className="collapse-content"> 
+    <p>Listing your bike for sale is free. We only charge a small fee if your bike successfully sells on our platform.</p>
+  </div>
+</div>
+<div className="collapse collapse-plus bg-base-200">
+  <input type="radio" name="my-accordion-3" /> 
+  <div className="collapse-title text-xl font-medium">
+  Is international shipping available for bikes?
+  </div>
+  <div className="collapse-content"> 
+    <p>Shipping options may vary depending on the sellers location and shipping preferences. You can discuss shipping arrangements with the seller directly.</p>
+  </div>
+</div>
+            </div>
         </div>
 
     );
