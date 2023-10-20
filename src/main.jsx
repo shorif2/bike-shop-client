f9273a9d1f2c76d3cb461c5504d6524edfd92542
Shorif <shorifahamed571@gmail.com>
2023-10-20 11:00:54 +0600

    Theme change function added

--- src/main.jsx
+++ src/main.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
       },
       {
         path:'/update-product/:id',
-        element: <UpdateProduct></UpdateProduct>,
+        element: <PrivetRouter><UpdateProduct></UpdateProduct></PrivetRouter>,
         loader: ({params})=> fetch(`http://localhost:5000/product/${params.id}`)
       },
       {
@@ -68,7 +68,7 @@ const router = createBrowserRouter([
 
       {
         path: '/view/:id',
-        element: <View></View>,
+        element: <PrivetRouter><View></View></PrivetRouter>,
         loader: ({params})=> fetch(`http://localhost:5000/product/${params.id}`)
       }
     ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  // daisyui: {
+  //   themes: ["light", "dark", "cupcake", "cyberpunk"],
+  // },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+      "cyberpunk"
+    ],
+  },
   theme: {
     extend: {},
   },
